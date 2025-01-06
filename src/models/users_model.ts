ad3becfa827da1ab1d94d487b3c03ba11ac9055a
inbal <inbalbarak123@gmail.com>
2025-01-06 20:36:55 +0200

    add login logic

---
+++ src/models/users_model.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+export interface UserAttributes {
+  username?: string;
+  email: string;
+  password: string;
+  imageUrl?: string;
+  _id?: string;
+  refreshToken?: string[];
+}
+
+const userSchema = new mongoose.Schema<UserAttributes>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  refreshToken: {
+    type: [String],
+    default: [],
+  },
+});
+
+const UserModel = mongoose.model<UserAttributes>("Users", userSchema);
+export default UserModel;
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      Users:
+ *         type: object
+ *         required:
+ *            - email
+ *            - username
+ *          properties:
+ *            email:
+ *              type: string
+ *              description: The user email
+ *            username:
+ *              type: string
+ *              description: The user username
+ *          example:
+ *            email: 'usernameexample'
+ *            username: '123456'
+ */
